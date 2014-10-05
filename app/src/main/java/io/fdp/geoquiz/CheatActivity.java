46088383375b5766c9848742f085d5d42aaf8c59
Fernando Paredes <nano@fdp.io>
2014-10-04 18:05:32 -0700

    Add basic CheatActivity class

---
+++ app/src/main/java/io/fdp/geoquiz/CheatActivity.java
@@ -0,0 +1,16 @@
+package io.fdp.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nano on 10/4/14.
+ */
+public class CheatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
